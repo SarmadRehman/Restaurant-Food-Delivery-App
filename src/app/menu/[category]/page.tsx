5787ac8f23a9d339f1bc12be4ce08d394f9292fb
Sarmad Rehman <sarmadr02@gmail.com>
2023-09-22 13:40:07 -0700

    folders and pages to begin

---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryPage = () => {
+  return (
+    <div>
+      Categorypage
+    </div>
+  )
+}
+
+export default CategoryPage
---
+++ src/orders/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrdersPage = () => {
+  return (
+    <div>
+      this is order oage
+    </div>
+  )
+}
+
+export default OrdersPage
